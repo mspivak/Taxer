dc4a15d05d7b817199153e9244e33b324ba53257
Mauro Spivak <mauro@coalitiontechnologies.com>
2015-08-05 13:14:59 -0300

    Migrating to Zip-Tax.com

--- src/Mauro/Taxer/Taxer.php
+++ src/Mauro/Taxer/Taxer.php
@@ -4,15 +4,60 @@ use Config;
 
 class Taxer {
 
-	static protected $uri = 'https://taxrates.api.avalara.com:443/postal?country=%s&postal=%s&apikey=%s';
+	static protected $uri = 'http://api.zip-tax.com/request/v20?key=%s&postalcode=%s&format=JSON';
 
-	static function  zip($zip, $country = 'usa') {
+	static function zip($zip, $city = null) {
 
-		$url = sprintf(self::$uri, $country, $zip, urlencode(Config::get('taxer::key')) );
+		$url = sprintf(self::$uri, Config::get('taxer::key'), $zip);
 
-		$rate_object = json_decode( file_get_contents( $url ) );
+		$rates = json_decode( file_get_contents( $url ) );
 
-		return (float) $rate_object->totalRate;
+		$rate_object = self::getTaxRateFromSet( $rates->results, $city );
+
+		return (float) round($rate_object->taxSales, 2);
 	}
+
+	static function getTaxRateFromSet( $set, $city ) {
+
+		if (!is_null($city)) {
+			if ($tax_rate = self::getTaxRateFromSetUsingCity( $set, $city )) {
+				return $tax_rate;
+			}
+		}
+
+		return self::getTaxRateFromSetUsingMax( $set );
+
+	}
+
+	static function getTaxRateFromSetUsingCity( $set, $city ) {
+
+		foreach ($set as $rate) {
+
+			if (strtolower($rate->geoCity) == strtolower($city)) {
+				return $rate;
+			}
+
+		}
+
+		return null;
+
+	}
+
+	static function getTaxRateFromSetUsingMax( $set ) {
+		
+		$maxRate = null;
+		foreach ($set as $rate) {
+		
+			if (is_null($maxRate) 
+			||	$rate->taxSales > $maxRate->taxSales) {
+				$maxRate = $rate;
+			}
+		
+		}
+		
+		return $maxRate;
+
+	}
+
 
 }
